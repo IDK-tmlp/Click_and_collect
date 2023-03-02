219a78f35be0b83ad2cecb7d11fdcdb2238d73a4
IDK-tmlp <50327904+IDK-tmlp@users.noreply.github.com>
2023-03-02 14:09:55 +0100

    Add proba pull

--- clicker/js/gacha.js
+++ clicker/js/gacha.js
@@ -1,11 +1,23 @@
 function pull(nombre) {
   if (money >= nombre * 1000) {
     let tab = [];
-    for (let i = 0; i < nombre; i++) {
-      tab.push(i);
-      money -= 1000;
+    for (let i = 1; i < nombre + 1; i++) {
+      dropRarity = Math.random() * 100;
+      if (dropRarity <= 3) {
+        tab.push(`${i} : SSR 3%`);
+        money -= 1000;
+      } else if (dropRarity <= 10) {
+        tab.push(`${i} : SR 7%`);
+        money -= 1000;
+      } else if (dropRarity <= 40) {
+        tab.push(`${i} : R 30%`);
+        money -= 1000;
+      } else if (dropRarity <= 100) {
+        tab.push(`${i} : C 60%`);
+        money -= 1000;
+      }
     }
-    alert(`You pulled ${tab}`);
+    alert(`${tab}`);
   } else {
     alert("Not enougth money");
   }
